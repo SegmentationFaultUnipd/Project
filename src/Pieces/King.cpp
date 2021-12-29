9d1007e39efd59ec787d37205da95ee9d4afc518
Filippo Niero <filippo.01.niero@gmail.com>
2021-12-29 21:30:54 +0100

    Continued to work on king

--- src/Pieces/King.cpp
+++ src/Pieces/King.cpp
@@ -1,7 +1,9 @@
 #include "King.h"
-
 bool chess::King::canMove(short to_file, short to_rank, Piece* board[8][8]) {
-    short delta_file = abs(to_file - file());
+    
+	//FIXME: i need the position of the enemies to check if the king is in check (call isInCheck method)
+	//FIXME: If we want the check function to be inside gamemanager or in board, we would still need a reference  
+	short delta_file = abs(to_file - file());
     short delta_rank = abs(to_rank - rank());
 
 	//King can move at maximum 1 square in all the directions
@@ -9,7 +11,9 @@ bool chess::King::canMove(short to_file, short to_rank, Piece* board[8][8]) {
 		return false;
 	}
 	const Piece* landing_piece = board[to_file][to_rank];
+	
 	return landing_piece == nullptr || landing_piece->color() != this->color();
+	//TODO check that by moving the king in a particular spot, the king is not in check. If he is in check, then return false
 };
 
 bool chess::King::canMove(Piece* board[8][8]) {
@@ -44,4 +48,21 @@ std::vector<chess::Coordinates> chess::King::legalMoves(Piece* board[8][8]) {
     	}
     }
     return moves;
+}
+
+
+bool chess::King::canCastle(short to_file, short to_rank, Piece* board[8][8],std::vector<Coordinates> enemy_positions) {
+	if(hasMoved()) {
+		return false;
+	}
+	//TODO Check that the to_file and to_rank are actually the positions of the towers, based also on the right color
+	//TODO check that the tower has not moved yet
+	//TODO Check that the squares in between are empty
+	//TODO Check that the king is not in check for every position
+	return true;
+}
+
+
+bool chess::King::isInCheck(short to_file, short to_rank, Piece* board[8][8],std::vector<Coordinates> enemy_positions) {
+
 }
--- src/Pieces/King.h
+++ src/Pieces/King.h
@@ -7,10 +7,16 @@
 namespace chess {
 
     class King : public Piece {
+		private:
+			bool hasMoved;
         public:
-            King(short file, short rank, Color color) : Piece{file, rank, color} {};
-            King(char file, short rank, Color color) : Piece{file, rank, color} {};
+            King(short file, short rank, Color color) : Piece{file, rank, color} {hasMoved = false;};
+            King(char file, short rank, Color color) : Piece{file, rank, color} {hasMoved = false;};
 
+			bool hasMoved() {return hasMoved;}
+			void firstMove() {hasMoved = true;}
+			bool canCastle(short to_file, short to_rank, Piece* board[8][8],std::vector<Coordinates> enemy_positions);
+			bool isInCheck(short to_file, short to_rank, Piece* board[8][8], std::vector<Coordinates> enemy_positions); //return true if the king would be in check in a particular position. Temporarily modifies the board before restoring it in 
             bool canMove(short to_file, short to_rank, Piece* board[8][8]) override;
             bool canMove(Piece* board[8][8]) override;
             std::vector<Coordinates> legalMoves(Piece* board[8][8]) override;
