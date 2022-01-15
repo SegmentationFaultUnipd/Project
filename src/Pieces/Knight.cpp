a27ac80b25fc25ba527a25ec144ac5c40f99210d
Mazzarotto Filippo <filippomazzarotto@gmail.com>
2022-01-15 02:13:25 +0100

    Big refactoring

--- src/Pieces/Knight.cpp
+++ src/Pieces/Knight.cpp
@@ -1,48 +1,62 @@
 #include "Knight.h"
 
-bool chess::Knight::canMove(Coordinates coords, chess::Board& board) const {
-    short delta_file = coords.file - file();
-    short delta_rank = coords.rank - rank();
+int manhattanDistance(chess::Coordinates coords1, chess::Coordinates coords2) {
+    return abs(coords1.file - coords2.file) + abs(coords1.rank - coords2.rank);
+}
 
-    //Knight moves with 3 total steps, so the manahattan distance must be 3
-    if (abs(delta_file) + abs(delta_rank) == 3 && delta_file != 0 && delta_rank != 0) {
-        return board.isEmpty(coords) || board.at(coords).color() != this->color();
-    }
-    return false;
+bool chess::Knight::canMoveAt(Coordinates landing_square, chess::Board &board) const
+{
+    return manhattanDistance(position_, landing_square) == 3
+        && landing_square.file != file()
+        && landing_square.rank != rank()
+        && board.isReachableBy(landing_square, *this);
 };
 
-bool chess::Knight::canMove(chess::Board& board) const {
-    for(const Coordinates& candidate : candidateMoves_())
-        if (candidate.inBounderies(0,7,0,7) && canMove(candidate, board))
+bool chess::Knight::canMove(chess::Board &board) const
+{
+    for (const Coordinates &landing_square : candidateMoves_())
+        if (board.isReachableBy(landing_square, *this))
             return true;
+
     return false;
 };
 
-std::vector<chess::Coordinates> chess::Knight::legalMoves(Board& board) const {
+std::vector<chess::Coordinates> chess::Knight::legalMoves(Board &board) const
+{
     std::vector<Coordinates> moves;
 
-    for(const Coordinates& candidate : candidateMoves_())
-        if (candidate.inBounderies(0,7,0,7) && canMove(candidate, board))
-            moves.push_back(candidate);
+    for (const Coordinates &landing_square : candidateMoves_())
+        if (board.isReachableBy(landing_square, *this))
+            moves.push_back(landing_square);
+
     return moves;
 }
 
-/*
-std::vector<chess::Coordinates> chess::Knight::takeablePieces(Board& board) const {
-    std::vector<Coordinates> pieces;
+std::vector<chess::Piece*> chess::Knight::takeablePieces(Board& board) const
+{
+    std::vector<Piece*> pieces;
 
-    for(Coordinates& candidate : candidateMoves_())
-        if (candidate.inBounderies(0,7,0,7) && canMove(candidate, board) && !board.at(candidate).isEmpty())
-            pieces.push_back(candidate);
+    for(const Coordinates& landing_square : candidateMoves_())
+        if (board.isTakingAPiece(landing_square, *this))
+            pieces.push_back(&board.at(landing_square));
+
     return pieces;
-}*/
+}
 
-// Constant time
-const std::vector<chess::Coordinates> chess::Knight::candidateMoves_() const{
-    return {
-      {file() + 1, rank() + 2}, {file() + 2, rank() + 1}, //Up-right
-      {file() + 1, rank() - 2}, {file() + 2, rank() - 1}, //Down-right
-      {file() - 1, rank() + 2}, {file() - 2, rank() + 1}, //Up-left
-      {file() - 1, rank() - 2}, {file() - 2, rank() - 1}, //Down-left
+const std::vector<chess::Coordinates> chess::Knight::candidateMoves_() const
+{
+    const std::vector<Coordinates> raw_moves {
+        {file() + 1, rank() + 2}, {file() + 2, rank() + 1}, // Up-right
+        {file() + 1, rank() - 2}, {file() + 2, rank() - 1}, // Down-right
+        {file() - 1, rank() + 2}, {file() - 2, rank() + 1}, // Up-left
+        {file() - 1, rank() - 2}, {file() - 2, rank() - 1}, // Down-left
     };
+
+    std::vector<Coordinates> moves_in_boundaries{};
+
+    for (Coordinates move : raw_moves)
+        if (move.inBounderies())
+            moves_in_boundaries.push_back(move);
+
+    return moves_in_boundaries;
 }
