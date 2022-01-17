7ec071308145a0666d7b3a34a62c6316dd960869
Filippo Niero <filippo.01.niero@gmail.com>
2022-01-17 23:23:35 +0100

    Refactoring

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,13 +1,5 @@
 #include "GameManager.h"
 
-chess::GameManager::GameManager(Player* player1, Player* player2, int max_moves) {
-	player1_ = player1;
-	player2_ = player2;
-	
-	max_moves_ = max_moves;
-
-	createLogFile();
-}
 
 void chess::GameManager::createLogFile() {
 	time_t now = time(0);
@@ -17,8 +9,8 @@ void chess::GameManager::createLogFile() {
 	file_name = "chesslog_" + padTime(now_localtime->tm_mday) + padTime(now_localtime->tm_mon) + year + "_" + padTime(now_localtime->tm_hour) + padTime(now_localtime->tm_min);
 	//Save stream, overwrite file if it already exists
 	log_stream.open(file_name, std::ofstream::trunc);
-	log_stream<<"PLAYER1:"<<player1_->getColor()<<std::endl;
-	log_stream<<"PLAYER2:"<<player2_->getColor()<<std::endl;
+	log_stream<<"PLAYER1:"<<player1_.getColor()<<std::endl;
+	log_stream<<"PLAYER2:"<<player2_.getColor()<<std::endl;
 }
 
 std::string chess::GameManager::padTime(int x) {
@@ -29,7 +21,7 @@ std::string chess::GameManager::padTime(int x) {
 }
 
 void chess::GameManager::nextPlayer() {
-	if(current_player == player1_) {
+	if(current_player.getColor() == player1_.getColor()) {
 		current_player = player2_;
 	} else {
 		current_player = player1_;
@@ -38,7 +30,7 @@ void chess::GameManager::nextPlayer() {
 
 void chess::GameManager::play() {
 	current_move = 0;//Contatore delle mosse: serve per le partite tra due PC, perché devono finire dopo max_moves mosse
-	current_player = (player1_->getColor() == WHITE)? player1_:player2_; //Seleziona il giocatore iniziale
+	current_player = (player1_.getColor() == WHITE)? player1_:player2_; //Seleziona il giocatore iniziale
 	bool infinite_game = (max_moves_ == -1);
 	bool isGameEnded = false;
 
@@ -46,11 +38,11 @@ void chess::GameManager::play() {
 		Coordinates from, to;
 		bool isValid = false;
 
-		std::cout << "Tocca al " << (current_player->getColor() == WHITE ? "bianco\n" : "nero\n");
+		std::cout << "Tocca al " << (current_player.getColor() == WHITE ? "bianco\n" : "nero\n");
 
 		do {
-			current_player->nextTurn(board, from, to);
-			isValid = board.move(from, to) && (board.at(to).color() == current_player->getColor());
+			current_player.nextTurn(board, from, to);
+			isValid = !board.isEmpty(from) && (board.at(from).color() == current_player.getColor()) && board.move(from, to);
 			if (!isValid)
 				std::cout << "Mossa non consentita\n";
 		} while (!isValid);
@@ -60,7 +52,7 @@ void chess::GameManager::play() {
 
 		// Promozione
 		if(board.at(to).ascii()=='P' && (to.rank == 0 || to.rank == 7) ) {
-			char chosen = current_player->choosePromotion();
+			char chosen = current_player.choosePromotion();
 			board.promote(to, chosen);
 			logPromotion(chosen, to);
 		}
@@ -68,7 +60,7 @@ void chess::GameManager::play() {
 		nextPlayer();
 		
 		//Check if player has available moves
-		std::list<Coordinates> pieces = board.getPiecesCoords(current_player->getColor());
+		std::list<Coordinates> pieces = board.getPiecesCoords(current_player.getColor());
 		bool player_has_at_least_1_move = false;
 		for(auto coords: pieces) {
 			if(board.legalMovesOf(board.at(coords)).size() > 0) {
@@ -77,17 +69,17 @@ void chess::GameManager::play() {
 			}
 		}
 		if(!player_has_at_least_1_move) {
-			if(board.isKingInCheck(current_player->getColor())) {
+			if(board.isKingInCheck(current_player.getColor())) {
 				//Scacco matto
 				log_stream<<"---"<<std::endl;
-				log_stream<<((current_player == player1_)?"Player1":"Player2")<<" has no valid moves and his king is in check";
+				log_stream<<((current_player.getColor() == player1_.getColor())?"Player1":"Player2")<<" has no valid moves and his king is in check";
 				nextPlayer();
 				win(current_player);
 			}else {
 				//Parità
 				log_stream<<"---"<<std::endl;
-				log_stream<<((current_player == player1_)?"Player1":"Player2")<<" has no valid moves but his king is not in check";
-				win(nullptr);
+				log_stream<<((current_player.getColor() == player1_.getColor())?"Player1":"Player2")<<" has no valid moves but his king is not in check";
+				draw();
 			}
 		}
 		if(!infinite_game) {
@@ -98,21 +90,21 @@ void chess::GameManager::play() {
 
 	if(current_move >= max_moves_) {
 		std::cout << "Number of moves reached. " << std::endl;
-		win(nullptr);
+		draw();
 	}
 	//TODO close everything, free up memory etc
 	cleanUp();
 }
 
-void chess::GameManager::win(Player* winner) {
-	if(winner == nullptr) {
-		std::cout << "Draw! " <<std::endl;
+void chess::GameManager::win(Player &winner) {
+	std::string win_message_part = (winner.getColor() == player1_.getColor())? "PLAYER 1": "PLAYER 2";
+	std::cout << "The winner is "<<win_message_part<<"!!!"<<std::endl;
+	log_stream<<"The winner is "<<win_message_part<<"!!!"<<std::endl;
+}
+
+void chess::GameManager::draw() {
+	std::cout << "Draw! " <<std::endl;
 		log_stream<<"Draw! "<<std::endl;
-	} else {
-		std::string win_message_part = (winner == player1_)? "PLAYER 1": "PLAYER 2";
-		std::cout << "The winner is "<<win_message_part<<"!!!"<<std::endl;
-		log_stream<<"The winner is "<<win_message_part<<"!!!"<<std::endl;
-	}
 }
 
 void chess::GameManager::cleanUp() {
--- src/GameManager.h
+++ src/GameManager.h
@@ -10,26 +10,33 @@ namespace chess {
 
     class GameManager {
         public:
-            GameManager(Player* player1, Player* player2, int max_moves);
+			//Nota: current_player viene inizializzato a player1 nel costruttore, ma una volta che viene chiamato GameManager::play() viene corretto con il giocatore che ha il bianco
+            GameManager(Player &player1, Player &player2, int max_moves) : player1_(player1), player2_(player2), max_moves_(max_moves), current_player(selectStartingPlayer(player1, player2)) {
+				createLogFile();
+			}
            	void play();
 			void logMove(Coordinates from, Coordinates to);
 			void logPromotion(char piece, Coordinates position);
 			
         private:
-			Player *player1_, *player2_;
+			Player &player1_, &player2_;
 			Board board;
 			int max_moves_;//If max_moves is -1, go on forever
 			int current_move = 0;
-			Player* current_player;
+			Player &current_player;
 			std::string file_name;
 			std::ofstream log_stream;
 
 			void nextPlayer();
-			void win(Player* winner);
+			void win(Player &winner);
+			void draw();
 			void createLogFile();
 			std::string padTime(int x);
 			void cleanUp();
 
+			static Player& selectStartingPlayer(Player& player1, Player& player2) {
+				return (player1.getColor()== WHITE)?player1: player2;
+			}
 		
     };
 
