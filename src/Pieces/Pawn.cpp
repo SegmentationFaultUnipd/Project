601ecaeaed43f29fd7086e513bd708343e19a30e
Filippo Niero <filippo.01.niero@gmail.com>
2022-01-23 17:36:42 +0100

    Update Pawn.cpp

--- src/Pieces/Pawn.cpp
+++ src/Pieces/Pawn.cpp
@@ -3,25 +3,25 @@
 
 void chess::Pawn::move(Coordinates new_position, Board& board) {
     has_moved = true;
-    short color_n;
+    short delta_rank;
     if(this->color() == WHITE) {
-        color_n = 1;
+        delta_rank = 1;
     }
     else {
-        color_n = -1;
+        delta_rank = -1;
     }
 
-    bool two_up = new_position.file == position_.file && new_position.rank == (position_.rank + (2 * color_n));
+    bool two_up = new_position.file == position_.file && new_position.rank == (position_.rank + (2 * delta_rank));
     if(two_up) {
         Coordinates from, to;
-        to = {position_.file, position_.rank + (1 * color_n)};
+        to = {position_.file, position_.rank + (1 * delta_rank)};
         //If there is a pawn on the right it can then en pass
-        from = {position_.file + 1, position_.rank + (2 * color_n)};
+        from = {position_.file + 1, position_.rank + (2 * delta_rank)};
         if(from.inBounderies() && board.isOppositeColor(from, this->color()) && board.at(from).ascii() == 'P') {
             board.addAvailableEnPassant(from, to);
         }
         //If there is a pawn on the left it can then en pass
-        from = {position_.file - 1, position_.rank + (2 * color_n)};
+        from = {position_.file - 1, position_.rank + (2 * delta_rank)};
         if(from.inBounderies() && board.isOppositeColor(from, this->color()) && board.at(from).ascii() == 'P'){
             board.addAvailableEnPassant(from, to);
         }
@@ -47,28 +47,28 @@ bool chess::Pawn::canMoveAt(Coordinates coords, Board& board) const {
     }
 
     //Check the pawn color
-    short color_n;
+    short delta_rank;
     if(this->color() == WHITE) {
-        color_n = 1;
+        delta_rank = 1;
     }
     else {
-        color_n = -1;
+        delta_rank = -1;
     }
     //The pawn is moving two steps forward (if it hasn't yet moved)
-    bool two_up = coords.file == position_.file && coords.rank == (position_.rank + (2 * color_n));
-    if(two_up && !has_moved && board.isEmpty(coords) && board.isEmpty({coords.file, position_.rank + (1 * color_n)})) {
+    bool two_up = coords.file == position_.file && coords.rank == (position_.rank + (2 * delta_rank));
+    if(two_up && !has_moved && board.isEmpty(coords) && board.isEmpty({coords.file, position_.rank + (1 * delta_rank)})) {
         return true;
     }
 
     //The pawn is moving one step forward (if the tile is free)
-    bool one_up = coords.file == position_.file && coords.rank == (position_.rank + (1 * color_n));
+    bool one_up = coords.file == position_.file && coords.rank == (position_.rank + (1 * delta_rank));
     if(one_up && board.isEmpty(coords)) {
         return true;
     }
 
     //The pawn is moving diagonally (if there's a opponent piece to take)
-    bool diag_right = coords.file == (position_.file + 1) && coords.rank == (position_.rank + (1 * color_n));
-    bool diag_left = coords.file == (position_.file - 1) && coords.rank == (position_.rank + (1 * color_n));
+    bool diag_right = coords.file == (position_.file + 1) && coords.rank == (position_.rank + (1 * delta_rank));
+    bool diag_left = coords.file == (position_.file - 1) && coords.rank == (position_.rank + (1 * delta_rank));
     if((diag_right || diag_left) && board.isOppositeColor(coords, this->color())) {
         return true;
     }
@@ -78,33 +78,26 @@ bool chess::Pawn::canMoveAt(Coordinates coords, Board& board) const {
 
 bool chess::Pawn::canMove(Board& board) const {
     //Check the pawn color
-    short color_n;
-    if(this->color() == WHITE) {
-        color_n = 1;
-    }
-    else {
-        color_n = -1;
-    }
+    short delta_rank = (color() == WHITE)?1:-1;
     //The pawn can move two steps forward
-    Coordinates double_up {position_.file, position_.rank + (2 * color_n)};
+    Coordinates double_up {position_.file, position_.rank + (2 * delta_rank)};
     if(double_up.inBounderies() && canMoveAt(double_up, board) && !board.isKingInCheckAfterMove(position_, double_up)) {
-            return true;
+		return true;
     }
-    if(position_.rank - 1 >= 0) {
-        //The pawn can move one step
-        Coordinates single_up {position_.file, position_.rank - 1};
-        bool one_step = single_up.inBounderies() && canMoveAt(single_up, board);
-        //The pawn can move diagonally
-        Coordinates diag_right {position_.file + 1, position_.rank + (1 * color_n)};
-        bool eat_right = diag_right.inBounderies() && canMoveAt(diag_right, board);
-        Coordinates diag_left {position_.file - 1, position_.rank + (1 * color_n)};
-        bool eat_left = diag_left.inBounderies() && canMoveAt(diag_left, board);
-        if (one_step && !board.isKingInCheckAfterMove(position_, single_up)
-            || eat_right && !board.isKingInCheckAfterMove(position_, diag_right)
-            || eat_left && !board.isKingInCheckAfterMove(position_, diag_left)) {
-            return true;
-        }
-    }
+    //Single move up
+	Coordinates single_up {position_.file, position_.rank + delta_rank};
+	if(single_up.inBounderies() && canMoveAt(single_up, board)) {
+		return true;
+	}
+	//Diagonal eating moves
+	Coordinates diag_right {position_.file + 1, position_.rank + delta_rank};
+	if(diag_right.inBounderies() && canMoveAt(diag_right, board)) {
+		return true;
+	}
+	Coordinates diag_left {position_.file - 1, position_.rank + delta_rank};
+	if(diag_left.inBounderies() && canMoveAt(diag_left, board)) {
+		return true;
+	}
 
     return false;
 }
@@ -112,35 +105,27 @@ bool chess::Pawn::canMove(Board& board) const {
 std::vector<chess::Coordinates> chess::Pawn::legalMoves(Board& board) const {
     std::vector<chess::Coordinates> moves = {};
     //Check the pawn color
-    short color_n;
-    if(this->color() == WHITE) {
-        color_n = 1;
-    }
-    else {
-        color_n = -1;
-    }
+    short delta_rank = (color() == WHITE)?1:-1;
     //Double move up
-    Coordinates double_up {position_.file, position_.rank + (2 * color_n)};
-    if(position_.rank - 2 >= 0 && double_up.inBounderies() && canMoveAt(double_up, board)) {
+    Coordinates double_up {position_.file, position_.rank + (2 * delta_rank)};
+    if(double_up.inBounderies() && canMoveAt(double_up, board)) {
         moves.push_back(double_up);
     }
 
-    if(position_.rank - 1 >= 0) {
-        //Single move up
-        Coordinates single_up {position_.file, position_.rank - 1};
-        if(single_up.inBounderies() && canMoveAt(single_up, board)) {
-            moves.push_back(single_up);
-        }
-        //Diagonal eating moves
-        Coordinates diag_right {position_.file + 1, position_.rank + (1 * color_n)};
-        if(diag_right.inBounderies() && canMoveAt(diag_right, board)) {
-            moves.push_back(diag_right);
-        }
-        Coordinates diag_left {position_.file - 1, position_.rank + (1 * color_n)};
-        if(diag_left.inBounderies() && canMoveAt(diag_left, board)) {
-            moves.push_back(diag_left);
-        }
-    }
+	//Single move up
+	Coordinates single_up {position_.file, position_.rank + delta_rank};
+	if(single_up.inBounderies() && canMoveAt(single_up, board)) {
+		moves.push_back(single_up);
+	}
+	//Diagonal eating moves
+	Coordinates diag_right {position_.file + 1, position_.rank + delta_rank};
+	if(diag_right.inBounderies() && canMoveAt(diag_right, board)) {
+		moves.push_back(diag_right);
+	}
+	Coordinates diag_left {position_.file - 1, position_.rank + delta_rank};
+	if(diag_left.inBounderies() && canMoveAt(diag_left, board)) {
+		moves.push_back(diag_left);
+	}
 
     return moves;
 }
